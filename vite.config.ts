e70bc4ddab3a48fb17d8527b1139062403b7b6ae
Nitzan Hen <code@nitzanhen.net>
2021-10-24 12:05:31 +0000

    add lint-all script and lint.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -20,4 +20,4 @@ export default defineConfig({
       }
     }
   },
-})
+});
