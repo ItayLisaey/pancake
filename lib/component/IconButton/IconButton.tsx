beec7c9d9efcc5b3cec6ff5408915feda8560165
Nitzan Hen <code@nitzanhen.net>
2021-10-27 14:28:27 +0000

    feat(IconButton): implemented IconButton.

---
+++ lib/component/IconButton/IconButton.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames';
+import { ReactNode } from 'react';
+import { pcake } from '../../utils/strings';
+import { BaseButton, BaseButtonProps } from '../BaseButton';
+import classes from './icon-button.module.scss';
+
+export interface IconButtonProps extends BaseButtonProps {
+	children: ReactNode
+}
+
+export const IconButton: React.VFC<IconButtonProps> = ({ className, ...baseButtonProps }) => (
+	<BaseButton className={classNames(pcake(), classes.root, className)} {...baseButtonProps} />
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BaseButton } from '../lib';
+import { IconButton } from '../lib/component/IconButton';
 import { TextButton } from '../lib/component/TextButton';
 import { Heli } from './Heli';
 
@@ -11,6 +12,7 @@ function App() {
       <TextButton style={{ color: 'white' }} postText={<Heli />}>BUTTON</TextButton>
       <TextButton preText={<Heli />} style={{ color: 'white' }} postText={<Heli />}>BUTTON</TextButton>
       <TextButton preText={<Heli />} style={{ color: 'white', fontSize: '30px' }} postText={<Heli />}>BUTTON</TextButton>
+      <IconButton style={{ color: 'white' }}><Heli /></IconButton>
     </>
   );
 }
