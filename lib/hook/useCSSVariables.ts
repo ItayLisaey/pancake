feed488832a16209c8c64fb9890263bc356ce52f
Nitzan Hen <code@nitzanhen.net>
2021-10-30 09:36:21 +0000

    lint(useCSSVariables)

--- lib/hook/useCSSVariables.ts
+++ lib/hook/useCSSVariables.ts
@@ -1,21 +1,18 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo } from 'react';
 import { kebabCase, pcake, randomString } from 'utils/strings';
 
 export const useCSSVariables = (vars: Record<string, string>, classPrefix = `${pcake()}-vars`) => {
   const className = useMemo(() => `${classPrefix}-${randomString(5)}`, [classPrefix]);
-
-  const stylesRef = useRef<HTMLStyleElement | null>(null);
 
   useEffect(() => {
     const styleTag = document.createElement('style');
     document.head.appendChild(styleTag);
 
     const cssRules = Object.values(vars).map(([key, value]) => `--${kebabCase(key)}: ${value};`).join('\n');
-    styleTag.sheet!.insertRule(`.${className} {\n${cssRules}\n}`)
-
+    styleTag.sheet!.insertRule(`.${className} {\n${cssRules}\n}`);
 
     return () => styleTag.remove();
-  }, []);
+  }, [className, vars]);
 
   return className;
 };
