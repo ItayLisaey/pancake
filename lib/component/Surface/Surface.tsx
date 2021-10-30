5b34a440bf9fdfd421988991356681d114467b4f
Nitzan Hen <code@nitzanhen.net>
2021-10-30 13:28:08 +0000

    feat(Surface): swatch functionality in Surface

--- lib/component/Surface/Surface.tsx
+++ lib/component/Surface/Surface.tsx
@@ -1,13 +1,28 @@
-import { JSXElementConstructor } from 'react';
+import { JSXElementConstructor, useMemo } from 'react';
 import classNames from 'classnames/dedupe';
 import { pcake } from 'utils/strings';
 import { HTMLElementProps } from 'common/HTMLElementProps';
+import { useSwatch, useTheme } from 'theme';
+import { useCSSVariables } from 'hook/useCSSVariables';
+import { BaseComponentProps } from 'common';
 import classes from './surface.module.scss';
 
-export interface SurfaceProps extends HTMLElementProps {
+export interface SurfaceProps extends HTMLElementProps, BaseComponentProps {
 	component?: string | JSXElementConstructor<any>
 }
 
-export const Surface: React.VFC<SurfaceProps> = ({ component: Component = 'div', className, ...props }) => (
-	<Component className={classNames(pcake(), classes.root, className)} {...props} />
-);
+export const Surface: React.VFC<SurfaceProps> = ({ component: Component = 'div', className, swatch: swatchKey, ...props }) => {
+	const theme = useTheme();
+	const [swatch, isDefault] = useSwatch(swatchKey);
+
+	const cssVars = useMemo(() => (isDefault ? {} : {
+		'pcakeSurfaceBackground': swatch.lighter,
+		'pcakeSurfaceCurvature': theme.curvature,
+	}) as Record<string, string>, [swatch, isDefault, theme]);
+
+	const cssVarsClass = useCSSVariables(cssVars);
+
+	return (
+		<Component className={classNames(pcake(), cssVarsClass, classes.root, className)} {...props} />
+	);
+};
