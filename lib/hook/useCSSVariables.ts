0f6848cb521ca238986cf5e387c79c105f135314
Nitzan Hen <code@nitzanhen.net>
2021-10-30 08:59:54 +0000

    Rewrite useCSSVariables with useEffect()

--- lib/hook/useCSSVariables.ts
+++ lib/hook/useCSSVariables.ts
@@ -1,4 +1,4 @@
-import { useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { kebabCase, pcake, randomString } from 'utils/strings';
 
 export const useCSSVariables = (vars: Record<string, string>, classPrefix = `${pcake()}-vars`) => {
@@ -6,17 +6,16 @@ export const useCSSVariables = (vars: Record<string, string>, classPrefix = `${p
 
   const stylesRef = useRef<HTMLStyleElement | null>(null);
 
-  stylesRef.current?.remove();
+  useEffect(() => {
+    const styleTag = document.createElement('style');
+    document.head.appendChild(styleTag);
 
-  const newStyleTag = document.createElement('style');
-  document.head.appendChild(newStyleTag);
+    const cssRules = Object.values(vars).map(([key, value]) => `--${kebabCase(key)}: ${value};`).join('\n');
+    styleTag.sheet!.insertRule(`.${className} {\n${cssRules}\n}`)
 
-  const cssRules = Object.values(vars).map(([key, value]) => `--${kebabCase(key)}: ${value};`).join('\n');
-  const sheet = newStyleTag.sheet!;
-  sheet.deleteRule(0);
-  sheet.insertRule(cssRules, 0);
 
-  stylesRef.current = newStyleTag;
+    return () => styleTag.remove();
+  }, []);
 
   return className;
 };
