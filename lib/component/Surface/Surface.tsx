4bce574f46fc1237e4cdbc930dd4457cbfc5da5e
Nitzan Hen <code@nitzanhen.net>
2021-12-12 22:18:06 +0000

    fix(Surface): `inline-block` pos. & `block` prop

--- lib/component/Surface/Surface.tsx
+++ lib/component/Surface/Surface.tsx
@@ -7,16 +7,22 @@ import { useThemeBinding } from 'hook';
 import classes from './surface.module.scss';
 
 export interface SurfaceProps extends HTMLElementProps, BaseComponentProps {
-	component?: string | JSXElementConstructor<any>
+	/** Which component to use for the surface */
+	component?: string | JSXElementConstructor<any>; 
+	/** 
+	 * By default, Surface is set to have `inline-block` positioning.
+	 * The `block` prop sets it to `block`, instead.
+	 */
+	block?: boolean;
 }
 
-export const Surface: React.VFC<SurfaceProps> = ({ component: Component = 'div', className, swatch: swatchKey, ...props }) => {
+export const Surface: React.VFC<SurfaceProps> = ({ component: Component = 'div', className, swatch: swatchKey, block, ...props }) => {
 	const cssVarsClass = useThemeBinding('Surface', swatchKey, (swatch, theme) => ({
 		background: swatch.lighter,
 		curvature: theme.curvature
 	}));
 
 	return (
-		<Component className={classNames(pcake, cssVarsClass, classes.root, className)} {...props} />
+		<Component className={classNames(pcake, cssVarsClass, classes.root, block && classes.block, className)} {...props} />
 	);
 };
